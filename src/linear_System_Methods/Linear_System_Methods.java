bf0b3a2fef37d42df448f9c432230a2b1b94abd4
Gianluca Pablo Sarmento de Mello <gianluca.mello@nddigital.local>
2019-10-02 15:00:47 -0300

    Adicionado o método de Gauss com Pivoteamento Total

--- src/linear_System_Methods/Linear_System_Methods.java
+++ src/linear_System_Methods/Linear_System_Methods.java
@@ -11,7 +11,7 @@ public class Linear_System_Methods {
 		// b = new float[]{5,3,-1};
 
 		matriz = new double[][] 
-				/**{{ 9, -6, -3, -4, -5, -5, -8, 6, -2, 2, -7, -1, 4, -9, 4, 5, 9, 4, 9, -5, -15.13620337 },
+				{{ 9, -6, -3, -4, -5, -5, -8, 6, -2, 2, -7, -1, 4, -9, 4, 5, 9, 4, 9, -5, -15.13620337 },
 				{ -8, 5, -7, -8, 0, 2, 10, -8, -2, -8, 10, -2, -10, 2, 7, -7, 6, -8, -4, -8, -39.00404466 },
 				{ 0, -1, -3, 9, -5, -4, -7, 8, 0, -3, -1, -2, -8, 10, -1, 5, -9, 10, 1, -9, 97.77414194 },
 				{ 9, -10, 7, -10, -4, 7, 4, 9, 9, -2, 2, 10, -2, 1, 2, 8, 4, -4, 6, 5, 77.49451566 },
@@ -30,12 +30,12 @@ public class Linear_System_Methods {
 				{ -10, -2, 10, 7, -7, 5, -8, 6, 5, 8, 9, -7, -5, -10, 7, -1, -2, -4, 2, 7, 154.6761739 },
 				{ -6, 6, 1, 4, 0, -2, 3, -8, 9, -6, 7, -6, -4, -2, 10, 7, 10, 1, 1, 2, 9.713142717 },
 				{ 1, -8, 8, -9, 10, 7, -5, 5, -2, -9, 10, 5, 6, 0, -8, -8, -3, 7, -7, -1, -57.9061427 },
-				{ 6, 9, -7, -4, -2, 0, 3, 1, -9, -7, 0, -8, 1, -5, -8, -4, 10, -3, 8, 6, -126.9036292 } };*/
+				{ 6, 9, -7, -4, -2, 0, 3, 1, -9, -7, 0, -8, 1, -5, -8, -4, 10, -3, 8, 6, -126.9036292 } };
 
 		
-		 { { 2, 2, 1, 1, 7 }, { 1, -1, 2, -1, 1 }, { 3, 2, -3, -2, 4 }, { 4, 3, 2, 1,12 } };
+		 /*{ { 2, 2, 1, 1, 7 }, { 1, -1, 2, -1, 1 }, { 3, 2, -3, -2, 4 }, { 4, 3, 2, 1,12 } };*/
 
-		gauss_Elimination_Partial_Pivoting(matriz);
+		gauss_Elimination_Total_Pivoting(matriz);
 	}
 
 	// Gauss Elimination Method
@@ -135,6 +135,91 @@ public class Linear_System_Methods {
 			System.out.format("x%d = %.5f\n", i, b[i]);
 		}
 	}
+	
+	private static void gauss_Elimination_Total_Pivoting(double matrix[][]) {
+		String[] vetorX = new String[matrix.length];
+		String auxX;
+		int l = 0, c=0;
+		double mult, sum, max = 0;
+		double[] b = new double[matrix.length];
+		double aux;
+
+		for(int i=0; i<vetorX.length; i++) {
+			vetorX[i] = "x"+i;
+		}
+		
+		System.out.println("Matrix");
+		for (int i = 0; i < matrix.length; i++) {
+			System.out.println("Iteração: "+i);
+			printMatrix(matrix);
+			System.out.println("Zerando a coluna " + i);
+			for (int j = 0; j < matrix.length; j++) {
+				
+				//Verifica qual o maior valor entre os coeficiêntes da matriz
+				if (i == j && i != matrix.length - 1) {
+					max = 0;
+					for (int x = i; x < matrix.length; x++) {
+						for (int y = j; y<matrix.length; y++) {
+							if (Math.abs(matrix[x][y]) > max) {
+								max = Math.abs(matrix[x][y]);
+								l = x;
+								c = y;
+							}
+						}
+					}
+					System.out.println(" Linha: " + l);
+					System.out.println("Coluna: " + c);
+					System.out.println("J: " + j);
+					System.out.println("O maior valor é: " + max);
+					//Substitui as colunas para deixar o maior valor como pivo na coluna atual
+					if(j != c) {
+						for(int y = 0; y < matrix.length; y++) {
+							aux = matrix[y][j];
+							matrix[y][j] = matrix[y][c];
+							matrix[y][c] = aux;
+							
+						}
+						auxX = vetorX[c];
+						vetorX[c] = vetorX[j];
+						vetorX[j] = auxX;
+					}
+					//substitui as linhas para deixar o maior valor como pivo
+					if (i != l) {
+						for (int x = i; x < matrix[0].length; x++) {
+							aux = matrix[i][x];
+							matrix[i][x] = matrix[l][x];
+							matrix[l][x] = aux;
+						}
+					}
+					
+					printMatrix(matrix);
+					System.out.println("-----------------------------------------");
+				}
+				
+				//Zera os coeficiêntes abaixo da diagonal principal
+				if (i < j) {
+					mult = matrix[j][i] / matrix[i][i];
+					for (int k = 0; k < matrix[0].length; k++) {
+						matrix[j][k] = matrix[j][k] - mult * matrix[i][k];
+					}
+				}
+			}
+		}
+		printMatrix(matrix);
+		b[matrix.length - 1] = matrix[matrix.length - 1][matrix.length] / matrix[matrix.length - 1][matrix.length - 1];
+		for (int i = matrix.length - 1; i >= 0; i--) {
+			sum = 0;
+			for (int j = i + 1; j < matrix.length; j++) {
+				sum += matrix[i][j] * b[j];
+			}
+			b[i] = (matrix[i][matrix.length] - sum) / matrix[i][i];
+		}
+		System.out.println("\nThe solution is: ");
+		for (int i = 0; i < b.length; i++) {
+			System.out.format("%s = %.5f\n", vetorX[i], b[i]);
+		}
+	}
+	
 
 	
 	private static void printMatrix(double matrix[][]) {
